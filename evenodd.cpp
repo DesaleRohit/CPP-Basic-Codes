e655950ba27527885025fdfaa6dfe10424c578e1
DesaleRohit <rohitdesale2005@gmail.com>
2024-08-18 12:30:18 +0530

    [ADDED]:Rohit - Cpp program to Find the Number is Even or Odd

---
+++ evenodd.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){ 
+    int a;
+    cout<<"Enter a number: ";
+    cin>>a;
+    if(a%2==0)
+    {
+        std::cout<<"Number is Even";
+    }
+    else
+    {   
+        std::cout<<"Number is Odd";   
+    }
+}
