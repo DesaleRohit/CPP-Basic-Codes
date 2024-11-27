611e2e33abd5f95dd76b14f7110b2a759563456d
DesaleRohit <rohitdesale2005@gmail.com>
2024-11-27 20:15:47 +0530

    [ADDED]:Rohit -Demonstration of math Functions

---
+++ Math_Function.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double a = 9.0, b = 16.0;
+
+    // Power and Square Root
+    cout << "Square root of 9: " << sqrt(a) <<endl;
+    cout << "2 raised to 3: " << pow(2.0, 3.0) <<endl;
+
+    // Trigonometry
+    cout << "Cosine of 45 degrees: " << cos(M_PI / 4) <<endl;
+
+    // Logarithms
+    cout << "Natural log of e: " << log(M_E) <<endl;
+
+    // Rounding
+    cout << "Ceil of 2.3: " << ceil(2.3) <<endl;
+    cout << "Floor of 2.9: " << floor(2.9) <<endl;
+
+    return 0;
+}
