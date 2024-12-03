ee65c95972a1b1d76fa3fb28a2e836bc87c19aee
DesaleRohit <rohitdesale2005@gmail.com>
2024-12-03 10:40:32 +0530

    [ADDED]:Rohit -This code for Two D Array

---
+++ TowD_Array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[2][2] = {{10, 20}, {30, 40}};
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            cout << "arr[" << i << "][" << j << "] =" << arr[i][j] << "\n";
+        }
+    }
+    return 0;
+}
