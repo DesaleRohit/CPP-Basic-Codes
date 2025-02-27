ca52a2d20db23d2245d6823b101a92c32533ab03
DesaleRohit <rohitdesale2005@gmail.com>
2025-02-27 23:45:25 +0530

    [ADDED]:Rohit - This code for Advance Foor Loop in C++

---
+++ AdvanceForLoop.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 1, j = 5, k = 1; i <= 5; i++, j--, k++)
+    {
+        cout << i << "  " << j << "  " << k << endl;
+    }
+}
